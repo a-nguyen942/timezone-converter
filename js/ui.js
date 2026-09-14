890014110c4cc3050bc36e74ceea33e13197d409
a-nguyen942 <281822810+a-nguyen942@users.noreply.github.com>
2026-09-13 19:42:58 -0700

    Restored changes

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,11 @@
 // ui changes
+
+// check if user has selected a secondary city
+// have bool variable second_city_selected = true/false 
+
+// if second_city_selected = false; 
+// display special UI for right timezone, only hyphens
+// hyphens should be all the same color and sizing, only appearing to show nothing is selected yet
+
+// else if second_city_selected = true;
+// display regular UI for right timezone, city, city name, country, time, weather/temp, etc.
