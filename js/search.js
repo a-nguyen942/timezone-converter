1971ea93e0fc8d75720d79a9e6221ac5bda33de5
a-nguyen942 <281822810+a-nguyen942@users.noreply.github.com>
2026-09-13 19:14:25 -0700

    Added js folder with multiple js files whose names show clear purpose

---
+++ js/search.js
@@ -0,0 +1,27 @@
+// add in dataset of just countries to search through to avoid searching through the entire dataset of countries and cities
+
+// data structure to hold recently searched countries
+
+
+// use eventListener to check if the user is typing into the search bar, if so run code below 
+// check if the search input is empty, if so have dropdown display recently searched countries/cities
+function checkRecentSearches(searchInput)
+{
+    // if empty, display recent searches
+    // else call normalizeInput
+}
+
+// user starts typing and normalizeInput runs
+function normalizeInput(searchInput)
+{
+    // normalize input to all lowercase and turn any characters with accents to base characters
+    // pass normalized input to filterCountries to begin filtering through dataset of countries/cities
+}
+
+// normalized input is passed to filterCountries to begin filtering through dataset of countries/cities
+function filterCountries(normalizedInput)
+{
+    // wittle down countries that don't contain letters the user gave
+    // display this to dropdown list of countries/cities that match the input
+}
+
---
+++ js/timezone.js
@@ -0,0 +1,1 @@
+// handle timezone calculations and conversions
